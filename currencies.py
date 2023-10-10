7155b79d1ebda6f3a242f1e3650cd6118a3b67ed
Tanushri Nayak <tanushri0310003@gmail.com>
2023-10-10 11:45:54 +0200

    Data structures challenge completed.

--- currencies.py
+++ currencies.py
@@ -1,11 +1,15 @@
 # pylint: disable=missing-docstring
-
+#import math
 # TODO: add some currency rates
-RATES = {}
+RATES = {"USDEUR":0.85, "GBPEUR":1.13, "CHFEUR":0.86, "EURGBP": 0.885}
 
 def convert(amount, currency):
     """returns the converted amount in the given currency
     amount is a tuple like (100, "EUR")
     currency is a string
     """
-    pass  # YOUR CODE HERE
+    lookup = amount[1]+currency
+    if lookup in RATES:
+        return round(amount[0] * RATES.get(lookup))
+    else:
+        return None
