d936c13ee6679b1e443177b20ed88defc1a188f0
Tanushri Nayak <tanushri0310003@gmail.com>
2023-10-10 11:59:52 +0200

    Refactored.

--- currencies.py
+++ currencies.py
@@ -1,17 +1,9 @@
 # pylint: disable=missing-docstring
-#import math
-# TODO: add some currency rates
 RATES = {"USDEUR":0.85, "GBPEUR":1.13, "CHFEUR":0.86, "EURGBP": 0.885}
 
 def convert(amount, currency):
     """returns the converted amount in the given currency
     amount is a tuple like (100, "EUR")
     currency is a string
-
-    lookup = amount[1]+currency
-    if lookup in RATES:
-        return
-    else:
-        return None
     """
     return round(amount[0] * RATES.get(amount[1]+currency)) if amount[1]+currency in RATES else None
